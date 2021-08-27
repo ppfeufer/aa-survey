f0b75ea0c93d4228c7a4c648d274af3beff19e33
Peter Pfeufer <info@ppfeufer.de>
2021-08-27 16:38:44 +0200

    views.py tests changed

--- aa_survey/tests/test_views.py
+++ aa_survey/tests/test_views.py
@@ -1,3 +1,7 @@
+"""
+Tests for view.py
+"""
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -6,9 +10,13 @@ from aa_survey.tests.utils import create_fake_user
 VIEWS_PATH = "aa_survey.views"
 
 
-class TestIndexViews(TestCase):
+class TestViews(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
+        """
+        Set up groups and users
+        """
+
         super().setUpClass()
 
         # User cannot access forum
@@ -26,35 +34,13 @@ class TestIndexViews(TestCase):
             permissions=["aa_survey.basic_access", "aa_survey.manage_survey"],
         )
 
-    # def setUp(self) -> None:
-    #     # board 1 has an unread topic
-    #     self.board_1 = Board.objects.create(name="Physics", category=self.category)
-    #     topic_1 = Topic.objects.create(subject="Mysteries", board=self.board_1)
-    #     create_fake_messages(topic_1, 4)
-    #     topic_1.update_last_message()
-    #     topic_2 = Topic.objects.create(subject="Recent Discoveries", board=self.board_1)
-    #     create_fake_messages(topic_2, 2)
-    #     topic_2.update_last_message()
-    #     LastMessageSeen.objects.create(
-    #         topic=topic_2,
-    #         user=self.user_1001,
-    #         message_time=topic_2.messages.order_by("-time_posted")[0].time_posted,
-    #     )
-    #     self.board_1.update_last_message()
-    #
-    #     # board 2 has no unread topics
-    #     self.board_2 = Board.objects.create(name="Math", category=self.category)
-    #     topic = Topic.objects.create(subject="Unsolved Problems", board=self.board_2)
-    #     create_fake_messages(topic, 2)
-    #     topic.update_last_message()
-    #     LastMessageSeen.objects.create(
-    #         topic=topic,
-    #         user=self.user_1001,
-    #         message_time=topic.messages.order_by("-time_posted")[0].time_posted,
-    #     )
-    #     self.board_2.update_last_message()
-
     def test_should_show_dashboard(self):
+        """
+        Test that a user with basic_access can see the bulletin board
+        :return:
+        :rtype:
+        """
+
         # given
         self.client.force_login(self.user_1002)
 
@@ -63,3 +49,19 @@ class TestIndexViews(TestCase):
 
         # then
         self.assertEqual(res.status_code, 200)
+
+    def test_should_not_show_dashboard(self):
+        """
+        Test that a user without basic_access can't see the surveys
+        :return:
+        :rtype:
+        """
+
+        # given
+        self.client.force_login(self.user_1001)
+
+        # when
+        res = self.client.get(reverse("aa_survey:dashboard"))
+
+        # then
+        self.assertIsNot(res.status_code, 200)
