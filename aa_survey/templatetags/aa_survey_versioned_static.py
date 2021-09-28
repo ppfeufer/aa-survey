2eab96bf8af649a4ede167a7d00dce89ee3e6755
Peter Pfeufer <info@ppfeufer.de>
2021-09-28 20:42:57 +0200

    [FIX] `versioned_static` renamed to avoid name collisions

--- aa_survey/templatetags/aa_survey_versioned_static.py
+++ aa_survey/templatetags/aa_survey_versioned_static.py
@@ -9,7 +9,7 @@ from aa_survey import __version__
 
 
 @register.simple_tag
-def versioned_static(path: str) -> str:
+def aa_survey_static(path: str) -> str:
     """
     Versioned static URL
     :param path:
